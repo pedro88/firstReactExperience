100c7963ac5e58b9a247b7de2fd629d7e1735b71
pedro88 <sandrep_85@hotmail.com>
2025-01-31 12:14:45 +0100

    gnn

---
+++ src/TodoList.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const Task = (props) => {
+    return (
+        <>
+         <input type="checkbox" /><label>  {props.task} </label>
+         <br></br>
+        </>
+    )
+}
+
+
+
+
+
+
+
+
+const TodoList = () => {
+    return (
+        <>
+        <header>
+            <h1>My Todo App</h1>
+        </header>
+        <main>
+            <form>
+                <input type='text' placeholder='Type a new todo' />
+            </form>
+            <button>
+                Add Todo
+            </button>
+            <hr></hr>
+            <section className="list">
+                <h2>Todos</h2>                
+                <Task task="trying new layout" />
+                <Task task="Youhou it's work!!" />
+                <Task task="I even add a 'br' to adding some style" />
+            </section>
+        </main>
+        </>
+    )
+}
+
+
+
+export default TodoList
